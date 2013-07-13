9b3a4c65682cafad4b0b179d1e41d35457a8a093
tobegit3hub <tobeg3oogle@gmail.com>
2013-07-13 11:16:38 +0800

    commit all files

---
+++ GoddessTheme/footer.php
@@ -0,0 +1,29 @@
+<hr>
+        <div class="container">
+             <footer class="footer">
+                <p class="pull-right"><a href="#">Top</a></p>
+                <p>Last update: <strong><?php $last = $wpdb->get_results("SELECT MAX(post_modified)
+AS MAX_m FROM $wpdb->posts WHERE (post_type = 'post' OR post_type
+= 'page') AND (post_status = 'publish' OR post_status = 'private')");
+$last = date('Y.n.j', strtotime($last[0]->MAX_m));echo $last; ?></strong></p>
+		
+                <ul class="breadcrumb">
+		<li>Power by  </li>	
+		  <li><a href="http://wordpress.org/" target="_blank">Wordpress</a> <span class="divider">/</span></li>
+		  <li><a href="http://twitter.github.io/bootstrap/" target="_blank">Bootstrap</a> <span class="divider">/</span></li>
+		  <li><a href="https://jquery.org/" target="_blank">JQuery</a><span class="divider">/</span></li>
+		  <li><a href="http://www.ubuntu.com/" target="_blank">Ubuntu</a><span class="divider">/</span></li>
+		  <li><a href="http://www.gnu.org/software/emacs/" target="_blank">Emacs</a><span class="divider">/</span></li>
+		  <li><a href="https://www.google.com/intl/en/chrome/browser/" target="_blank">Chrome</a><span class="divider">/</span></li>
+		  <li><a href="https://github.com/tobegit3hub/GoddessTheme.git" target="_blank">Github</a></li>
+		  
+		</ul>
+             </footer>
+        </div>
+ 
+    </div> <!-- /container -->
+ 
+    <?php wp_footer(); ?>
+ 
+  </body>
+</html>
---
+++ GoddessTheme/index.php
@@ -0,0 +1,38 @@
+
+
+<?php get_header(); ?>
+
+<div class="row">
+
+  <div class="span8">
+
+   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+	  <h1 class="post_title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1> 
+
+	<p class="post_time"><?php the_time('F jS, Y'); ?></p>
+
+	<div class="post_content"><?php the_content(); ?></div>
+
+	<hr />
+	
+   <?php endwhile; else: ?>
+	  <p><?php _e('太尴尬了::>_<::'); ?></p>
+	  <img src="http://www.chendihao.cn/wp-content/uploads/Pikachu.png" />
+   <?php endif; ?>
+   	 
+  </div>
+
+  <div class="span3 offset1">
+	<?php get_sidebar(); ?>
+  </div>
+  
+</div>
+
+<div class="postnav">
+	<?php if(function_exists('wp_pagenavi')){ wp_pagenavi(); } else { ?>
+	<div class="prev"><?php next_posts_link(__('« Previous Entries')) ?></div>
+	<div class="next"><?php previous_posts_link(__('Next Entries »')) ?></div> <?php } ?>
+</div>
+
+<?php get_footer(); ?>
